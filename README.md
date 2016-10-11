301bd2353f7c08aef364fabab84aba91a631f8db
JesseZhuang <408455428@qq.com>
2016-10-11 21:52:02 +0800

    20161011

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # angularjs2-demo
+
+创建： [angular-cli](https://cli.angular.io)
+
+安装： npm i angular-cli -g
+
+打包： [angular2-webpack-starter](https://github.com/AngularClass/angular2-webpack-starter/)
---
+++ webpack-starter/dev/main.js
@@ -0,0 +1,7 @@
+import {MyAppComponent} from "./components/hello/hello.js";
+
+import {HelloTemplateComponent} from "./components/helloTemplate/helloTemplate.js";
+
+import {Component, View, bootstrap} from '';
+
+bootstrap(MyAppComponent);
