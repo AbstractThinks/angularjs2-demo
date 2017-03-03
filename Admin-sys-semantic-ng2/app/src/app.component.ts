3016583a9ef87c7ce7d3286a35917c3ed8938187
emobossemo <408455428@qq.com>
2017-03-03 13:28:40 +0800

    20170303

--- Admin-sys-semantic-ng2/app/src/app.component.ts
+++ Admin-sys-semantic-ng2/app/src/app.component.ts
@@ -1,8 +1,24 @@
-import {Component}     from '@angular/core';
+import {
+	Component,
+	OnInit,
+	ViewChild,
+	ngAfterViewInit
+}  from '@angular/core';
 
 @Component({
     selector: 'my-app',
     template:  `<router-outlet></router-outlet>`,
 })
 
-export class AppComponent { }
+export class AppComponent implements OnInit { 
+	constructor() {
+		$('.pusher').dimmer('show')
+	}
+
+	ngOnInit():void {
+		
+	}
+	ngAfterViewInit(): void {
+		$('.pusher').dimmer('hide')
+	}
+}
--- Admin-sys-semantic-ng2/app/src/appContentModule/component/personnel/personnel.component.ts
+++ Admin-sys-semantic-ng2/app/src/appContentModule/component/personnel/personnel.component.ts
@@ -1,11 +1,21 @@
-import {Component} from '@angular/core';
-
+import {
+	Component,
+	ViewChild
+} from '@angular/core';
+import {BaseData} from '../../../../mockData/baseData';
 @Component({
   selector: 'personnel-component',
   templateUrl:  './personnel.component.html',
   // directives: [],
 })
 export class ContentPersonnelComponent {
+  basedata:any = BaseData
+	@ViewChild('addModal')
+    addModal:any;
 
+    handleRemove(e?:any):void {
+      console.log(e) //{title1: "1", title2: "2"}
+    	this.addModal.show()
+    }
 
 }
