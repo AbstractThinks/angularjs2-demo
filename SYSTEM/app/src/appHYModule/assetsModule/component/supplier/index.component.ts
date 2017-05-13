bb61019328d6bb7529d32587bb8032cc145c10f0
alvinqiu <alvinqiu@qq.com>
2017-05-13 18:33:21 +0800

    supplier page

--- SYSTEM/app/src/appHYModule/assetsModule/component/supplier/index.component.ts
+++ SYSTEM/app/src/appHYModule/assetsModule/component/supplier/index.component.ts
@@ -4,11 +4,8 @@ import {
   AfterViewInit
 } from '@angular/core';
 import { UrlService } from '../../../../appServiceModule/urlService.component';
-import {
-	Router,
-	ActivatedRoute,
-	Params
-} from "@angular/router";
+import { HYService } from '../../../../appServiceModule/HYService.component';
+
 
 @Component({
   selector: 'hy-assets-supplier-container',
@@ -16,18 +13,60 @@ import {
 
 })
 export class HYAssetsSupplierComponent {
-	
-	constructor (
-		private urlService: UrlService,
-		private aRoute: ActivatedRoute
-	) {
-		let that = this;
-		
-		
+	@ViewChild('infoModal')
+	infoModal:any;
+	infoDatas:any = {};
+	equipment:any = {};
+	shcools:any = [];
+	userProfile:any = {};
+	constructor (private urlService: UrlService,private hyService: HYService) {
 
 	}
 	ngAfterViewInit(): void {
-		
+		let that = this;
+
+		this.urlService.hy_req_get("api/schools?page=1").then((response:any) => {
+			that.shcools = response.json();
+		});
+		this.urlService.hy_req_get('api/profile').then((response:any) => {
+			that.userProfile = response.json();
+			this.urlService.hy_req_get(`api/equipment-supplier/${that.userProfile.schoolId}/1/3`).then((response:any) => {
+				that.infoDatas = response.json();
+			})
+			this.equipment.schoolId = that.userProfile.schoolId;
+		});
+	}
+	handleSave ():void {
+		let data = this.hyService.urlEncode(this.equipment).substr(1);
+		let that = this;
+
+		this.urlService.hy_req_post(`api/equipment-supplier?${data}`, this.equipment).then((response:any) => {
+			that.infoModal.hide();
+		});
+	}
+	onFirstHandler(e:any):void{
+		let that = this;
+		this.urlService.hy_req_get(`api/equipment-supplier/${that.userProfile.schoolId}/1/2`).then((response:any) => {
+			that.infoDatas = response.json();
+		})
+	}
+	onLastHandler(e:any):void{
+		let that = this;
+		this.urlService.hy_req_get(`api/equipment-supplier/${that.userProfile.schoolId}/${that.infoDatas.totalPage}/2`).then((response:any) => {
+			that.infoDatas = response.json();
+		})
+	}
+	onNextHandler(e:any):void{
+		let that = this;
+		this.urlService.hy_req_get(`api/equipment-supplier/${that.userProfile.schoolId}/${that.infoDatas.pageNumber+1}/2`).then((response:any) => {
+			that.infoDatas = response.json();
+		})
+	}
+	onPreviousHandler(e:any):void{
+		let that = this;
+		this.urlService.hy_req_get(`api/equipment-supplier/${that.userProfile.schoolId}/${that.infoDatas.pageNumber-1}/2`).then((response:any) => {
+			that.infoDatas = response.json();
+		})
 	}
 	
 }
