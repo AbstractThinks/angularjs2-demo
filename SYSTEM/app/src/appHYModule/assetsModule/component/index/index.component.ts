4365f1ae83d3977e758e6e0c781b0c3d9d3c256b
alvinqiu <alvinqiu@qq.com>
2017-06-11 21:57:17 +0800

    资产管理更新

--- SYSTEM/app/src/appHYModule/assetsModule/component/index/index.component.ts
+++ SYSTEM/app/src/appHYModule/assetsModule/component/index/index.component.ts
@@ -15,18 +15,22 @@ import { HYService } from '../../../../appServiceModule/HYService.component';
 export class HYAssetsIndexComponent {
   @ViewChild('infoModal')
   infoModal:any;
+  @ViewChild('quickMarkModal')
+  quickMarkModal:any;
   infoDatas:any = {};
+  quickMarkDatas:any = [];
   equipment:any = {};
   userProfile:any = {};
 	constructor (private urlService: UrlService,private hyService: HYService) {
 
 	}
 	ngAfterViewInit(): void {
-		let that = this;
+	    let that = this;
 		this.urlService.hy_req_get('api/profile').then((response:any) => {
 			that.userProfile = response.json();
 			this.urlService.hy_req_get(`api/equipment/${that.userProfile.schoolId}/1/2`).then((response:any) => {
 				that.infoDatas = response.json();
+                setTimeout(() => {$('.checkbox').checkbox()}, 0);
 			})
 		});
 	}
@@ -39,6 +43,23 @@ export class HYAssetsIndexComponent {
         });
     }
     handleMake ():void {
+        // let that = this;
+        // that.quickMarkModal.show({inverted: true});
+        //
+        // for (var i = 0; i < $('.checkbox.checked').length-1; i++) {
+        // $('.checkbox.checked input').attr('id')
+        // $('.checkbox.checked input').attr('name')
+        //     that.quickMarkDatas.push({"id":$('.checkbox.checked input').attr('id'),"name":$('.checkbox.checked input').attr('name')})
+        // }
+        //
+        // new QRCode('.quickMark', {
+        //     text: 'helloworld',
+        //     width: 256,
+        //     height: 256,
+        //     colorDark : '#000000',
+        //     colorLight : '#ffffff',
+        //     correctLevel : QRCode.CorrectLevel.H
+        // });
 
     }
     handlePrint (e:any):void {
